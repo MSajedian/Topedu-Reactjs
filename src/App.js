224be7e28540563135094763d8353022dbedacd8
MSajedian <mohammadsajedian@gmail.com>
2021-08-01 17:02:37 +0100

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import { Nav, Navbar, Form, FormControl, Button, NavDropdown } from 'react-bootstrap';
+// import { Nav, Navbar, Form, FormControl, Button, NavDropdown } from 'react-bootstrap';
 import {
   BrowserRouter as Router,
   Switch,
@@ -7,47 +7,13 @@ import {
   // Link,
 } from "react-router-dom";
 import LogInPage from './components/LogInPage'
-// import HomePage from './components/HomePage'
+import HomePage from './components/HomePage'
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <Navbar bg="light" expand="lg">
-          <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbarScroll" />
-          <Navbar.Collapse id="navbarScroll">
-            <Nav
-              
-              style={{ maxHeight: '100px' }}
-              navbarScroll
-            >
-              <Nav.Link href="#action1">Home</Nav.Link>
-              <Nav.Link href="#action2">Link</Nav.Link>
-              <NavDropdown title="Link" id="navbarScrollingDropdown">
-                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action5">Something else here</NavDropdown.Item>
-              </NavDropdown>
-              <Nav.Link href="#" disabled>
-                Link
-              </Nav.Link>
-            </Nav>
-            <Form
-              // className="d-flex"
-              className="d-flex ms-auto my-2 my-lg-0"
-            >
-              <FormControl
-                type="search"
-                placeholder="Search"
-                className="mr-2"
-                aria-label="Search"
-              />
-              <Button variant="outline-success">Search</Button>
-            </Form>
-          </Navbar.Collapse>
-        </Navbar>
+        
 
         {/* <Route exact path="/">
 
@@ -74,9 +40,9 @@ class App extends Component {
 
 
         <Switch>
-          {/* <Route exact path="/">
+          <Route exact path="/">
             <HomePage />
-          </Route> */}
+          </Route>
           <Route exact path="/log-in">
             <LogInPage />
           </Route>
