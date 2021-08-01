5b7467533acc74012bd35b7267a725a672a8f13d
MSajedian <mohammadsajedian@gmail.com>
2021-08-01 15:41:55 +0100

    TopEdu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
+import { Nav, Navbar, Form, FormControl, Button, NavDropdown } from 'react-bootstrap';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  // Link,
+} from "react-router-dom";
+import LogInPage from './components/LogInPage'
+// import HomePage from './components/HomePage'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              
+              style={{ maxHeight: '100px' }}
+              navbarScroll
+            >
+              <Nav.Link href="#action1">Home</Nav.Link>
+              <Nav.Link href="#action2">Link</Nav.Link>
+              <NavDropdown title="Link" id="navbarScrollingDropdown">
+                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action5">Something else here</NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link href="#" disabled>
+                Link
+              </Nav.Link>
+            </Nav>
+            <Form
+              // className="d-flex"
+              className="d-flex ms-auto my-2 my-lg-0"
+            >
+              <FormControl
+                type="search"
+                placeholder="Search"
+                className="mr-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Navbar>
+
+        {/* <Route exact path="/">
+
+          
+        </Route> */}
+
+        {/* <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/log-in">Log in</Link>
+              </li>
+            </ul>
+          </nav> */}
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+
+        {/* <Route exact path="/users">
+              <Users />
+            </Route> */}
+
+
+        <Switch>
+          {/* <Route exact path="/">
+            <HomePage />
+          </Route> */}
+          <Route exact path="/log-in">
+            <LogInPage />
+          </Route>
+        </Switch>
+      </Router>
+    );
+  }
 }
+
 
 export default App;
---
+++ src/components/HomePage.jsx
@@ -0,0 +1,45 @@
+import { Component } from 'react';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import { FcAbout } from 'react-icons/fc';
+
+class HomePage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+    render() {
+        return (
+            <>
+                <Container className="my-5" style={{ backgroundColor: "white", width: "50vw", padding: "10vw", border: "2px solid #b6b6b6", borderRadius: "5px" }}>
+                    <Row className="text-center">
+                        <FcAbout style={{ fontSize: 50 }} className="mb-3" />
+                        <b className="mb-4">Log in to TopEdu</b>
+                    </Row>
+                    <Row className="justify-content-center">
+                        <Col>
+                            <Form>
+                                <Form.Group className="mb-3" controlId="formBasicEmail">
+                                    <Form.Label>Email or username</Form.Label>
+                                    <Form.Control type="email" placeholder="" />
+                                    {/* <Form.Text className="text-muted">
+                                    We'll never share your email with anyone else.
+                                </Form.Text> */}
+                                </Form.Group>
+
+                                <Form.Group className="mb-3" controlId="formBasicPassword">
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control type="password" placeholder="" />
+                                </Form.Group>
+                                <Button variant="primary" type="submit">
+                                    Log in
+                                </Button>
+                            </Form>
+                        </Col>
+                    </Row>
+                </Container >
+            </>
+        );
+    }
+}
+
+export default HomePage;
