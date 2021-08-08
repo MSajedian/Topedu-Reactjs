bab3f88ab5717d8da9856c511ded01b9d44836a7
MSajedian <mohammadsajedian@gmail.com>
2021-08-08 16:02:36 +0100

    .

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -21,9 +21,9 @@ class HomePage extends Component {
                             <h1 className="display-3">COLLABORATIVE LEARNING PLATFORM</h1>
                             <p className="lead my-3">Learning experiences made easy, social and interactive
                                 Increase learner engagement & knowledge retention in higher education and corporate training settings
-                                Sign up for free</p>
+                            </p>
                         </div>
-                        <Col style={{ marginTop: "1rem" }}>
+                        <Col className="mt-4">
                             <a href="https://app.eduflow.com/sign-up" className="rounded" style={{ backgroundColor: "rgb(0,22,50)", color: "rgb(255,255,255)", padding: "1.7vw 3vw 2vw 3vw", textDecoration: "none", fontSize: "1.4rem", fontWeight: "bold" }}>Sign up for free &nbsp;
                                 <svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z" stroke="#fff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M12 16l4-4-4-4M8 12h8" stroke="#fff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>
                             </a>
