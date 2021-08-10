f8446ef06188b45567edd7caf973277481099a90
MSajedian <mohammadsajedian@gmail.com>
2021-08-10 17:24:13 +0100

    dashboard page created

---
+++ src/components/DashboardPage.jsx
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+// import { Nav, Navbar, Form, FormControl, Button } from 'react-bootstrap';
+// import { FcAbout } from 'react-icons/fc';
+// import { Container, Row, Col } from 'react-bootstrap';
+
+class DashboardPage extends Component {
+    render() {
+        return (
+           <>
+           <p></p>
+           </>
+        );
+    }
+}
+
+export default DashboardPage;
