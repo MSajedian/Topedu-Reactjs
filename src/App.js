098c95c1640e2fb318aad268ea8eb9a22183c58c
MSajedian <mohammadsajedian@gmail.com>
2021-10-01 12:58:54 +0100

    InvitationLink updated

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { ProvideAuth, PrivateRoute } from "./components/auth/UseAuth";
 import Main from './components/main/Main'
 import LogIn from './components/LogIn'
 import Signup from './components/Signup'
+import Join from './components/Join'
 import Home from './components/home/Home'
 import Courses from './components/courses/Courses'
 import CodePlayground from './components/courses/CodePlayground'
@@ -13,15 +14,15 @@ export default function App() {
   return (
     <ProvideAuth>
       <Router>
-          <Switch>
-            <Route exact path="/"> <Main /> </Route>
-            <Route path="/login"> <LogIn /> </Route>
-            <Route path="/signup"> <Signup /> </Route>
-            <Route path="/join"> <Signup /> </Route>
-            <PrivateRoute path="/home"> <Home /> </PrivateRoute>
-            <PrivateRoute path="/courses/:courseId"> <Courses /> </PrivateRoute>
-            <PrivateRoute path="/code-playground"> <CodePlayground /> </PrivateRoute>
-          </Switch>
+        <Switch>
+          <Route exact path="/"> <Main /> </Route>
+          <Route path="/login"> <LogIn /> </Route>
+          <Route path="/signup"> <Signup /> </Route>
+          <Route path="/join"> <Join /> </Route>
+          <PrivateRoute path="/home"> <Home /> </PrivateRoute>
+          <PrivateRoute path="/courses/:courseId"> <Courses /> </PrivateRoute>
+          <PrivateRoute path="/code-playground"> <CodePlayground /> </PrivateRoute>
+        </Switch>
       </Router>
     </ProvideAuth >
   );
