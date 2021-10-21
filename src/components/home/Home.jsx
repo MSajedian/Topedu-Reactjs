198927d6787031ba6469f5810f26503d2ad1dc77
MSajedian <mohammadsajedian@gmail.com>
2021-10-21 14:43:38 +0100

    Confirmation to delete courses added

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useDebugValue, useEffect, useState } from "react";
-import { Button, Card, Col, Container, Form, Modal, Placeholder, Row, Tab, Tabs, Alert } from 'react-bootstrap';
+import { Button, Card, Col, Container, Form, Modal, Placeholder, Row, Tab, Tabs, Alert, Badge } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import { useHistory } from "react-router-dom";
 import UseAuth from '../auth/UseAuth';
@@ -18,9 +18,17 @@ export default function Home() {
     const [newCourseTitle, setNewCourseTitle] = useStateWithLabel('', "newCourseTitle");
     const [userType, setUserType] = useStateWithLabel('', "userType");
 
+    // Create Course Modal
     const [showCreateCourseModal, setShowCreateCourseModal] = useStateWithLabel(false, "showCreateCourseModal");
     const handleCloseCreateCourseModal = () => setShowCreateCourseModal(false);
     const handleShowCreateCourseModal = () => setShowCreateCourseModal(true);
+
+    // Delete Course Modal
+    const [showDeleteCourseModal, setShowDeleteCourseModal] = useStateWithLabel(false, "showDeleteCourseModal");
+    const handleCloseDeleteCourseModal = () => setShowDeleteCourseModal(false);
+    const handleShowDeleteCourseModal = () => setShowDeleteCourseModal(true);
+    const [courseTitleToDelete, setCourseTitleToDelete] = useStateWithLabel('', "courseTitleToDelete");
+    const [courseIdToDelete, setCourseIdToDelete] = useStateWithLabel('', "courseIdToDelete");
 
     function useStateWithLabel(initialValue, name) {
         const [value, setValue] = useState(initialValue);
@@ -141,7 +149,7 @@ export default function Home() {
                         <div className="h1"><span className="fst-italic">Hello </span><span className="fst-italic" style={{ color: "#0022ff" }}>{userName}</span><span className="fst-italic">, good to see you again!</span>👋</div>
                     </div>
                     <div className="d-flex justify-content-center">
-                        <div>{userType === "admin" || userType === "instructor" ? <button className="btn-grad-blue" onClick={handleShowCreateCourseModal}>+ Create Course</button> : <></>}</div>
+                        <div>{userType === "admin" || userType === "instructor" ? <button className="btn-grad-blue mb-2" onClick={handleShowCreateCourseModal}>+ Create Course</button> : <></>}</div>
                     </div>
                     <Modal centered show={showCreateCourseModal} onHide={handleCloseCreateCourseModal}>
                         <Modal.Header closeButton>
@@ -181,7 +189,7 @@ export default function Home() {
                                                             }
                                                         </Card.Title>
                                                         {userType === "admin" || userType === "instructor" ?
-                                                            <Button id={`delete-button${index}`} className="btn btn-danger position-absolute bottom-0 start-0 m-1 button-delete" onClick={() => (deleteCourse(course._id))}>
+                                                            <Button id={`delete-button${index}`} className="btn btn-danger position-absolute bottom-0 start-0 m-1 button-delete" onClick={() => { setCourseTitleToDelete(course.title); setCourseIdToDelete(course._id); handleShowDeleteCourseModal() }}>
                                                                 <svg width="16px" height="16px" viewBox="0 0 16 16" ><path d="M2.03995183,3.5 L1,3.5 C0.723857625,3.5 0.5,3.27614237 0.5,3 C0.5,2.72385763 0.723857625,2.5 1,2.5 L5,2.5 L5,2 C5,1.17185763 5.67185763,0.5 6.5,0.5 L9.5,0.5 C10.3281424,0.5 11,1.17185763 11,2 L11,2.5 L15,2.5 C15.2761424,2.5 15.5,2.72385763 15.5,3 C15.5,3.27614237 15.2761424,3.5 15,3.5 L13.9600771,3.5 L13.0749738,14.1242112 L13.0749583,14.1243977 C13.0098846,14.9019827 12.3600634,15.5 11.58,15.5 L4.42067,15.5 C3.64057767,15.5 2.99070695,14.9019413 2.92572876,14.1242425 L2.03995183,3.5 Z M3.04342135,3.5 L3.92226386,14.0410691 C3.94394479,14.3005598 4.1606632,14.5 4.42067,14.5 L11.58,14.5 C11.8399987,14.5 12.0567206,14.3005553 12.0784417,14.0410023 L12.9566128,3.5 L3.04342135,3.5 Z M6,2.5 L10,2.5 L10,2 C10,1.72414237 9.77585763,1.5 9.5,1.5 L6.5,1.5 C6.22414237,1.5 6,1.72414237 6,2 L6,2.5 Z M7,11.5 C7,11.7761424 6.77614237,12 6.5,12 C6.22385763,12 6,11.7761424 6,11.5 L6,6.5 C6,6.22385763 6.22385763,6 6.5,6 C6.77614237,6 7,6.22385763 7,6.5 L7,11.5 Z M10,11.5 C10,11.7761424 9.77614237,12 9.5,12 C9.22385763,12 9,11.7761424 9,11.5 L9,6.5 C9,6.22385763 9.22385763,6 9.5,6 C9.77614237,6 10,6.22385763 10,6.5 L10,11.5 Z"></path></svg>
                                                             </Button>
                                                             : <></>
@@ -246,8 +254,24 @@ export default function Home() {
                             </Row>
                         </Tab>
                     </Tabs>
+                    <Modal show={showDeleteCourseModal} onHide={handleCloseDeleteCourseModal} >
+                        <Modal.Header closeButton>
+                            <Modal.Title>Course Deletion</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body >
+                            <div className="text-center">Do you want to delete this course?</div>
+                            <div className="text-center mt-2 fs-3">{courseTitleToDelete}</div>
+                        </Modal.Body>
+                        <Modal.Footer >
+                            <Button variant="secondary" onClick={handleCloseDeleteCourseModal}>
+                                No
+                            </Button>
+                            <Button variant="danger" onClick={() => { deleteCourse(courseIdToDelete); handleCloseDeleteCourseModal() }}>
+                                Yes
+                            </Button>
+                        </Modal.Footer>
+                    </Modal>
                 </Container>
-
             </div>
         </>
     );
