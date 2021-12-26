f7a78b656a7444c552648cc704fe5ca9cb0472cf
MSajedian <mohammadsajedian@gmail.com>
2021-12-26 02:35:03 +0000

    added 2 view to offcanvas code playground

--- src/components/OffCanvasCodePlayground.jsx
+++ src/components/OffCanvasCodePlayground.jsx
@@ -3,10 +3,11 @@ import { Offcanvas } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
 import { Container, Row, Col } from 'react-bootstrap';
 import Editor from "@monaco-editor/react";
+import { HiOutlineViewBoards } from "react-icons/hi";
 
 export default function OffCanvasCodePlayground({ ...props }) {
   const [show, setShow] = useState(false);
-
+  const [view, setView] = useState(4);
   const handleClose = () => setShow(false);
   const toggleShow = () => setShow((s) => !s);
   const [srcdocOfIframe, setSrcdocOfIframe] = useState(``)
@@ -26,37 +27,43 @@ export default function OffCanvasCodePlayground({ ...props }) {
       <Button variant="success" className="btn-grad-green px-3 py-2" onClick={toggleShow} >
         👨‍💻 Code Playground
       </Button>
-      <Offcanvas show={show} onHide={handleClose} {...props} scroll={true} backdrop={false} className="height-70vh">
+      <Offcanvas show={show} onHide={handleClose} {...props} scroll={true} backdrop={false} className="height-80vh">
         <Offcanvas.Header closeButton>
           <Offcanvas.Title>👨‍💻 Code Playground</Offcanvas.Title>
+          <Button variant="secondary" onClick={() => (setView(4))} className="mx-1 ms-auto">
+            <HiOutlineViewBoards size="1.5em" />
+          </Button>
+          <Button variant="secondary" onClick={() => (setView(12))} className="mx-1" >
+            <HiOutlineViewBoards size="1.5em" className="rotate90deg"  />
+          </Button>
         </Offcanvas.Header>
         <Offcanvas.Body className="pt-0">
           <Container>
             <Row>
-              <Col xs={12} md={4}>
+              <Col xs={12} md={view}>
                 <div className="text-center fw-bold">HTML</div>
                 <Editor
-                  height="200px"
+                  height="35vh"
                   defaultLanguage="html"
                   defaultValue={htmlTextAreaValue}
                   onChange={(value, event) => (setHtmlTextAreaValue(value))}
                   theme="vs-dark"
                 />
               </Col>
-              <Col xs={12} md={4}>
+              <Col xs={12} md={view}>
                 <div className="text-center fw-bold">CSS</div>
                 <Editor
-                  height="200px"
+                  height="35vh"
                   defaultLanguage="css"
                   defaultValue={cssTextAreaValue}
                   onChange={(value, event) => (setCssTextAreaValue(value))}
                   theme="vs-dark"
                 />
               </Col>
-              <Col xs={12} md={4}>
+              <Col xs={12} md={view}>
                 <div className="text-center fw-bold">JavaScript</div>
                 <Editor
-                  height="200px"
+                  height="35vh"
                   defaultLanguage="javascript"
                   defaultValue={jsTextAreaValue}
                   onChange={(value, event) => (setJsTextAreaValue(value))}
